b0efc7ea6145dc710a831de3d594fe1ad0cf0f34
fosahadal <fosahadal@protonmail.com>
2023-05-23 15:53:12 -0600

    dab spelling mongodb, dockerfiles

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -1,7 +1,7 @@
 import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
-    MONGOBD: Joi.required(),
+    MONGDB: Joi.required(),
     PORT: Joi.number().default(3005),
     DEFAULT_LIMIT: Joi.number().default(6),
 })
