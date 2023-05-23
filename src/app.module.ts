10691cf4ef25aadd5d2871e73116949e4c31ac6a
fosahadal <fosahadal@protonmail.com>
2023-05-23 16:26:44 -0600

    Update README

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { JoiValidationSchema } from './config/joi.validation';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot(process.env.MONGOBD),
+    MongooseModule.forRoot(process.env.MONGODB),
     PokemonModule,
     CommonModule,
     SeedModule
